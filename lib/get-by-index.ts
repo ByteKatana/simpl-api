7f6fbdb78feb83e98b4f0d1be9e9294befaafb51
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-01-17 17:13:48 +0300

    fix: any type related issues

--- lib/get-by-index.ts
+++ lib/get-by-index.ts
@@ -1,9 +1,9 @@
-export function getByIndex(index: any, fetchedData: object[]) {
+export function getByIndex(index: unknown, fetchedData: object[]) {
   if (Number(index) <= 0 || index === undefined) return { message: "Invalid Index" }
   else {
     if (index === "first") return fetchedData[0]
     if (index === "last") return fetchedData[fetchedData.length - 1]
     if (index === "random") return fetchedData[Math.floor(Math.random() * fetchedData.length)]
-    return fetchedData[Number(index - 1)]
+    return fetchedData[Number(index) - 1]
   }
 }
