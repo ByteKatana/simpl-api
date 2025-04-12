b51d5f206ee619e3f5224e35a227b398cd7b55a4
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-04-12 19:14:33 +0300

    🧪 test: Add tests for components/dashboard/entry-field.tsx

---
+++ __tests__/components/dashboard/entry-field.test.tsx
@@ -0,0 +1,104 @@
+import "@testing-library/jest-dom"
+import React, { MutableRefObject, useRef } from "react"
+import EntryField from "../../../components/dashboard/entry-field"
+import { render } from "@testing-library/react"
+import { before } from "node:test"
+
+const mockSetShowError = jest.fn()
+const mockSetFormErrors = jest.fn()
+const mockSetFormValues = jest.fn()
+
+describe("Check if EntryField component rendered properly", () => {
+  it("Render TextArea entry field properly", () => {
+    const mockField = {
+      mock_field: {
+        value_type: "string",
+        form_type: "textarea",
+        length: "101"
+      }
+    }
+    const mockKey = 1
+    const mockFormRef = {
+      current: {
+        mock_field: {
+          value_type: "string",
+          form_type: "textarea",
+          length: "101"
+        },
+        focus: jest.fn()
+      }
+    } as MutableRefObject<any>
+    const mockFormValues = {
+      name: "",
+      slug: "",
+      namespace: ""
+    }
+    const formErrors = {}
+    const mockShowError = {}
+    const mockFormErrors = {}
+    const mockShowErrors = true
+
+    const entryField = render(
+      <EntryField
+        key={mockKey}
+        field={mockField}
+        formRef={mockFormRef}
+        formValues={mockFormValues}
+        formErrors={mockFormErrors}
+        showErrors={mockShowErrors}
+        showError={mockShowError}
+        setFormErrors={mockSetFormErrors}
+        setShowError={mockSetShowError}
+        setFormValues={mockSetFormValues}
+      />
+    )
+
+    expect(entryField.getByRole("textbox"))
+  })
+  it("Render Input entry field properly", () => {
+    const mockField = {
+      mock_field: {
+        value_type: "string",
+        form_type: "input",
+        length: "101"
+      }
+    }
+    const mockKey = 1
+    const mockFormRef = {
+      current: {
+        mock_field: {
+          value_type: "string",
+          form_type: "textarea",
+          length: "101"
+        },
+        focus: jest.fn()
+      }
+    } as MutableRefObject<any>
+    const mockFormValues = {
+      name: "",
+      slug: "",
+      namespace: ""
+    }
+    const formErrors = {}
+    const mockShowError = {}
+    const mockFormErrors = {}
+    const mockShowErrors = true
+
+    const entryField = render(
+      <EntryField
+        key={mockKey}
+        field={mockField}
+        formRef={mockFormRef}
+        formValues={mockFormValues}
+        formErrors={mockFormErrors}
+        showErrors={mockShowErrors}
+        showError={mockShowError}
+        setFormErrors={mockSetFormErrors}
+        setShowError={mockSetShowError}
+        setFormValues={mockSetFormValues}
+      />
+    )
+
+    expect(entryField.getByRole("textbox"))
+  })
+})
