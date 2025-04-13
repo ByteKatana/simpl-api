f8969e0b3f6c0c1b6c1f378ea92b0d1f6e715fc9
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-04-13 15:48:34 +0300

    🧪 test: Add tests for controllers/entry-type.controller.ts

---
+++ __tests__/controllers/entry-type.controller.test.ts
@@ -0,0 +1,48 @@
+import "@testing-library/jest-dom"
+import { EntryType } from "../../interfaces"
+import { EntryTypeController } from "../../controllers/entry-type.controller"
+
+describe("Check if entry type controller handles action properly", () => {
+  let entryTypeController: EntryTypeController
+  let entryTypeData: EntryType
+  let entryTypeId: string
+
+  afterAll(() => {
+    jest.clearAllMocks()
+  })
+
+  it("Check if entry type created properly", async () => {
+    entryTypeData = {
+      name: "mock type",
+      namespace: "mock-type",
+      fields: []
+    }
+    entryTypeController = new EntryTypeController(entryTypeData, true)
+    const createEntryType = await entryTypeController.create()
+
+    expect(createEntryType.result).toEqual({ status: "success", message: "Entry Type has been created." })
+    entryTypeId = createUser.entryTypeId
+  })
+  it("Check if entry type updated properly", async () => {
+    entryTypeData = {
+      name: "mock type1",
+      namespace: "mock-type1",
+      fields: []
+    }
+    entryTypeController = new EntryTypeController(entryTypeData, true)
+    const updateEntryType = await entryTypeController.update(entryTypeId)
+
+    expect(updateEntryType).toEqual({ status: "success", message: "Entry Type has been updated." })
+  })
+  it("Check if entry type deleted properly", async () => {
+    entryTypeData = {
+      name: "mock type1",
+      namespace: "mock-type1",
+      fields: []
+    }
+    entryTypeController = new EntryTypeController(entryTypeData, true)
+    const deleteEntryType = await entryTypeController.delete(entryTypeId)
+
+    expect(deleteEntryType).toEqual({ status: "success", message: "Entry Type has been deleted." })
+  })
+})
