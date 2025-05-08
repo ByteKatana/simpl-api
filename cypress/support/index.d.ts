e20201b16564f4ae79ee71ec9826aa1ab8e98014
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-05-09 00:40:49 +0300

    🎨 refactor: enhance Cypress typings for custom commands
    Signed-off-by: ByteKatana <19264901+ByteKatana@users.noreply.github.com>

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,7 +1,16 @@
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    getDataTest(dataTestSelector: string): Chainable<JQuery<Node>>
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to select DOM element by data-testid attribute.
+       * @param dataTestSelector - The data-testid value to select
+       * @example cy.getDataTest("my-button")
+       */
+      getDataTest(dataTestSelector: string): Chainable<JQuery<HTMLElement>>
 
-    waitForRequest(): void
+      waitForRequest: () => void
+    }
   }
 }
+
+export {}
