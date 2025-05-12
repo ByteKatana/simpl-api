10b33ef54bb2491b5b5105b9e4924831299fd00e
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-05-12 23:49:11 +0300

    🧪 test: uncomment and enable test for fetching all users
    Signed-off-by: ByteKatana <19264901+ByteKatana@users.noreply.github.com>

--- __tests__/api/users/users.api.test.ts
+++ __tests__/api/users/users.api.test.ts
@@ -8,23 +8,23 @@ describe("/api/v1/users", () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
-  /*  it("GET / - should return all users", async () => {
-      const request = httpMocks.createRequest<NextApiRequest>({
-        method: "GET",
-        url: "/api/v1/users",
-        query: {
-          apikey: "dbbf764d6ce785adb15a184699867b24"
-        }
-      })
+  it("GET / - should return all users", async () => {
+    const request = httpMocks.createRequest<NextApiRequest>({
+      method: "GET",
+      url: "/api/v1/users",
+      query: {
+        apikey: "dbbf764d6ce785adb15a184699867b24"
+      }
+    })
 
-      const response = httpMocks.createResponse<NextApiResponse>()
+    const response = httpMocks.createResponse<NextApiResponse>()
 
-      await indexRouteHandler(request, response)
-      const data = await response._getJSONData()
-      expect(response._getStatusCode()).toBe(200)
-      expect(data.message).not.toBe({ message: "You're not authorized!" })
-      expect(Array.isArray(data)).toBe(true)
-    })*/
+    await indexRouteHandler(request, response)
+    const data = await response._getJSONData()
+    expect(response._getStatusCode()).toBe(200)
+    expect(data.message).not.toBe({ message: "You're not authorized!" })
+    expect(Array.isArray(data)).toBe(true)
+  })
 
   it("GET / - should return 'You're not authorized' if there is no API key ", async () => {
     const request = httpMocks.createRequest<NextApiRequest>({
