ada6e815355e174085065f0303ea6cb4929070f8
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2025-05-15 14:10:51 +0300

    🎨 refactor: add mockclient support and return statements API responses

--- pages/api/v1/entry-type/[slug].ts
+++ pages/api/v1/entry-type/[slug].ts
@@ -1,4 +1,4 @@
-import { NextApiResponse, NextApiRequest } from "next"
+import { NextApiRequest, NextApiResponse } from "next"
 import { apiBuilderController } from "../../../../controllers/api-builder.controller"
 import { apiKeyController } from "../../../../controllers/api-key.controller"
 
@@ -11,7 +11,7 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
   if (apiKeyData[0] !== undefined && apiKeyData[0].key === apikey) {
     const apiBuilder = new apiBuilderController("single-param", "entry_types", "namespace", slug)
 
-    res.status(200).json(await apiBuilder.fetchData("Equals"))
+    return res.status(200).json(await apiBuilder.fetchData("Equals"))
   }
-  res.status(200).json({ message: "You're not authorized!" })
+  return res.status(200).json({ message: "You're not authorized!" })
 }
