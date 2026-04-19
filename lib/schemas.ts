49e07655000fadc187d6505015bb67c46c5653a5
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2026-04-20 01:55:13 +0300

    ✨ feat: add extended schemas for entries, entry types, permissions, and privileges

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { UserStatus } from "@/interfaces"
+import { UserStatus, PublishStatus, CrudAction } from "@/interfaces"
 import { ObjectId } from "mongodb"
 
 //Constants
@@ -9,6 +9,13 @@ const MAX_PROFILE_IMG_WIDTH = 500
 const MAX_PROFILE_IMG_HEIGHT = 500
 
 const tempPermGroups = ["admin", "editor", "viewer"] // It will be replaced with the actual permission groups from the database(prisma)
+/*
+* PERSONAL NOTE:
+* Zod schema for permission groups/roles
+import { Role } from '@prisma/client';
+import { z } from 'zod';
+export const UserRoleSchema = z.nativeEnum(Role);
+* */
 
 /*
  * User Schemas
@@ -25,7 +32,7 @@ const UserProfileImgSchema = z
   })
   .refine(
     (file) =>
-      new Promise((resolve, reject) => {
+      new Promise((resolve) => {
         const reader = new FileReader()
         reader.onload = (e) => {
           const img = new Image()
@@ -146,4 +153,133 @@ export const UserDeleteSchema = z.object({
   ])
 })
 
+/*
+ * EntryType Schemas
+ * EntryTypeSchema -> Schema for creating a new entry type
+ * EntryTypeFieldSchema -> Schema for creating a new field in an entry type
+ * */
+export const EntryTypeFieldValidationSchema = z.object({
+  required: z.boolean().default(false),
+  minLength: z.coerce.number().min(0).optional(),
+  maxLength: z.coerce.number().min(1).optional(),
+  pattern: z
+    .string()
+    .optional()
+    .refine(
+      (val) => {
+        if (!val) return true
+        try {
+          new RegExp(val)
+          return true
+        } catch (e) {
+          return false
+        }
+      },
+      {
+        message: "Invalid regular expression pattern"
+      }
+    )
+})
+
+export const EntryTypeFieldSchema = z.object({
+  instanceId: z.string().min(1),
+  type: z.string().min(1),
+  name: z.string().optional(),
+  label: z.string().optional(),
+  placeholder: z.string().optional(),
+  options: z.array(z.string()).optional().default([]),
+  validation: EntryTypeFieldValidationSchema.optional(),
+  nextFieldId: z.string().optional()
+})
+
+export const EntryTypeFieldsetSchema = z.object({
+  instanceId: z.string().min(1),
+  name: z.string().optional(),
+  fields: z.array(EntryTypeFieldSchema).min(1, { message: "At least one field is required in a row" })
+})
+
+export const EntryTypeSchema = z.object({
+  name: z.string().min(1, { message: "Entry Type Name is required" }),
+  namespace: z.string().min(1, { message: "Namespace is required" }).default("itself"),
+  field_structure: z.array(EntryTypeFieldsetSchema).min(1, { message: "At least one row is required" })
+})
+
+/*
+ * Entry Schemas
+ * EntryCreateSchema -> Schema for creating a new entry
+ * EntryUpdateSchema -> Schema for updating an entry
+ * EntryDeleteSchema -> Schema for deleting an entry
+ * */
+export const EntryCreateSchema = z.object({
+  _id: z
+    .union([z.instanceof(ObjectId), z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })])
+    .optional(),
+  name: z.string().min(1, { message: "Entry Name is required" }),
+  entry_type: z.object({
+    _id: z.union([
+      z.instanceof(ObjectId),
+      z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })
+    ]),
+    name: z.string().min(1),
+    namespace: z.string().min(1)
+  }),
+  slug: z.string().min(1, { message: "Slug is required" }),
+  status: z.nativeEnum(PublishStatus).default(PublishStatus.Draft),
+  data: z.object(z.any()).optional(), // The actual field values of the entry
+  created_at: z.date().default(() => new Date()),
+  created_by: z.string().optional(),
+  updated_at: z.date().default(() => new Date()),
+  updated_by: z.string().optional()
+})
+
+export const EntryUpdateSchema = z.object({
+  _id: z.union([
+    z.instanceof(ObjectId),
+    z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })
+  ]),
+  name: z.string().min(1, { message: "Entry Name is required" }).optional(),
+  slug: z.string().min(1, { message: "Slug is required" }).optional(),
+  status: z.nativeEnum(PublishStatus).optional(),
+  data: z.object(z.any()).optional(),
+  updated_at: z.date().default(() => new Date()),
+  updated_by: z.string().optional()
+})
+
+export const EntryDeleteSchema = z.object({
+  _id: z.union([
+    z.instanceof(ObjectId),
+    z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })
+  ])
+})
+
+// Enum for CRUD actions
+
+export const CrudActionSchema = z.nativeEnum(CrudAction)
+
+// Privilege Schema
+export const PrivilegeSchema = z.object({
+  _id: z
+    .union([z.instanceof(ObjectId), z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })])
+    .optional(),
+  perm_group: z.string().min(1, "Permission Group is required"),
+  namespace: z.object({
+    _id: z.union([
+      z.instanceof(ObjectId),
+      z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })
+    ]),
+    namespace: z.string().min(1, "Namespace is required")
+  }),
+  action: CrudActionSchema,
+  privilege: z.string().min(1, "Privilege string is required")
+})
+
+// Permission Group Schema
+export const PermissionGroupSchema = z.object({
+  _id: z
+    .union([z.instanceof(ObjectId), z.string().refine((val) => ObjectId.isValid(val), { message: "Invalid ObjectId" })])
+    .optional(),
+  name: z.string().min(1, "Name is required"),
+  icon: z.string().optional(),
+  slug: z.string().min(1, "Slug is required"),
+  privileges: z.array(PrivilegeSchema).default([]) // Now strictly uses PrivilegeSchema
 })
